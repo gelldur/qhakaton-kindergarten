671129f50301a7c288a746b8d378bc99963651df
Dawid Drozd <drozddawid.uam@gmail.com>
2016-02-20 16:38:24 +0100

    Add Events AddChild and UpdateBeacon

---
+++ app/src/main/java/com/qhakaton/kindergarten/bus/event/EventAddChild.java
@@ -0,0 +1,10 @@
+package com.qhakaton.kindergarten.bus.event;
+
+import com.qhakaton.kindergarten.model.Child;
+
+/**
+ * Created by Dawid Drozd aka Gelldur on 20.02.16.
+ */
+public class EventAddChild {
+	public Child child;
+}
---
+++ app/src/main/java/com/qhakaton/kindergarten/bus/event/EventUpdateBeacon.java
@@ -0,0 +1,10 @@
+package com.qhakaton.kindergarten.bus.event;
+
+import com.qhakaton.kindergarten.model.Beacon;
+
+/**
+ * Created by Dawid Drozd aka Gelldur on 20.02.16.
+ */
+public class EventUpdateBeacon {
+	public Beacon beacon;
+}
